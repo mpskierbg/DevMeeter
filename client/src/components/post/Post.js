0b4df9ae9d7525126b02a1aaf2e16a78e9826ca9
matthew senko <msenko2@gmail.com>
2018-07-15 13:36:26 -0500

    added comment reply and comment feed

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
 import CommentForm from "./CommentForm";
+import CommentFeed from "./CommentFeed";
+import Posts from "../posts/Posts";
 
 class Post extends Component {
   componentDidMount() {
@@ -23,6 +25,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
